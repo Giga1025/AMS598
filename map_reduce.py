5bfc4ffb6b7a476a24ab139a1f9b0263919629b8
Yaswanth Reddy Bhuma <ybhuma@login2.cm.cluster>
2025-09-24 18:29:40 -0400

    Updated map_reduce.py and added mapper/reducer slurm scripts

--- map_reduce.py
+++ map_reduce.py
@@ -2,6 +2,7 @@ import os
 import glob
 from collections import Counter
 from multiprocessing import Pool
+import argparse
 
 # ------------ Mapper ----------------
 def mapper(file_path):
@@ -29,7 +30,7 @@ def reducer(file_list):
             for line in f:
                 k, v = line.strip().split()
                 total[k] += int(v)
-    
+
     reducer_id = os.path.basename(file_list[0]).replace("map_", "").split(".")[0]
     out_path = os.path.join(REDUCE_OUT_DIR, f"reduce_{reducer_id}.txt")
     with open(out_path, "w") as out:
@@ -55,8 +56,10 @@ def final_merge(reduce_files):
 
 # ------------ Main ----------------
 if __name__ == "__main__":
-
-    NETID = "ybhuma"
+    parser = argparse.ArgumentParser(description = "Mapreduce script with mapper and reducer stages")
+    parser.add_argument("--stage", choices =["mapper","reducer"], required = True, help = "Which stage to run: mapper / reducer / full")
+    args = parser.parse_args()
+    NETID = "Bhuma_YaswanthReddy"
 
     INPUT_DIR = "/gpfs/projects/AMS598/projects2025_data/project1_data/"
     MAP_OUT_DIR = f"/gpfs/projects/AMS598/class2025/{NETID}/map_outputs/"
@@ -65,15 +68,18 @@ if __name__ == "__main__":
     os.makedirs(MAP_OUT_DIR, exist_ok=True)
     os.makedirs(REDUCE_OUT_DIR, exist_ok=True)
 
-    files = glob.glob(os.path.join(INPUT_DIR, "*.txt"))
-    with Pool(processes=4) as pool:
-        map_outputs = pool.map(mapper, files)
+    if args.stage == "mapper":
+        files = glob.glob(os.path.join(INPUT_DIR, "*.txt"))
+        with Pool(processes=4) as pool:
+        	map_outputs = pool.map(mapper, files)
 
-    chunk_size = len(map_outputs)//4
-    reducer_inputs = [map_outputs[i:i+chunk_size] for i in range(0, len(map_outputs), chunk_size)]
+    if args.stage == "reducer":
+        map_outputs = glob.glob(os.path.join(MAP_OUT_DIR, "map_*.txt"))
+        chunk_size = len(map_outputs)//4
+        reducer_inputs = [map_outputs[i:i+chunk_size] for i in range(0, len(map_outputs), chunk_size)]
 
-    with Pool(processes=4) as pool:
-        reduce_outputs = pool.map(reducer, reducer_inputs)
+        with Pool(processes=4) as pool:
+            reduce_outputs = pool.map(reducer, reducer_inputs)
 
     # --- Final merge ---
-    final_merge(reduce_outputs)
+        final_merge(reduce_outputs)
